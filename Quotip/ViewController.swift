5c034a5f289948a6b73a669f8c610ae0d2d6d0d6
GautierBillard <56152915+swipip@users.noreply.github.com>
2020-05-17 16:37:30 +0200

    initial commit

---
+++ Quotip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotip
+//
+//  Created by Gautier Billard on 17/05/2020.
+//  Copyright © 2020 Gautier Billard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
